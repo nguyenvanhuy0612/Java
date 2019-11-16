be61cf2eb061b75d26b12f5c9a7179aef30296c2
Nguyen Van Huy <57367674+nguyenvanhuy0612@users.noreply.github.com>
2019-11-16 17:13:22 +0700

    Update

--- StringsSecondAssignments/Debug_part2.java
+++ StringsSecondAssignments/Debug_part2.java
@@ -8,19 +8,22 @@
 public class Debug_part2 {
     public void findAbc(String input){
         int index = input.indexOf("abc");
+        System.out.println("input.length "+input.length());
         while (true){
             if (index == -1 || index >= input.length() - 3){
                 break;
             }
+            System.out.println("index "+index);
             String found = input.substring(index+1, index+4);
             System.out.println(found);
-            index = input.indexOf("abc",index+4);
+            index = input.indexOf("abc",index+3);
+            System.out.println("index after updating " + index);
         }
     }
 
     public void test(){
         //findAbc("abcd");
-        findAbc("abcdabc");
+        findAbc("abcabcabcabca");
     }
 
 }
